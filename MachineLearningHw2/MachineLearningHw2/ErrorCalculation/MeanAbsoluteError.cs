00c3d4d57f97fcf29f6e42cfecdc6aa0c885e1da
Jorge <jogonzal@microsoft.com>
2017-04-29 19:19:45 -0700

    Add error calculation classes

---
+++ MachineLearningHw2/MachineLearningHw2/ErrorCalculation/MeanAbsoluteError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MachineLearningHw2.ErrorCalculation
+{
+	public static class MeanAbsoluteError
+	{
+		public static double Calculate(List<MoviePrediction> predictions)
+		{
+			double numerator = 0;
+			foreach (var moviePrediction in predictions)
+			{
+				double diff = moviePrediction.RealRating - moviePrediction.Prediction;
+				numerator += Math.Abs(diff);
+			}
+
+			return numerator / predictions.Count;
+		}
+	}
+}
---
+++ MachineLearningHw2/MachineLearningHw2/ErrorCalculation/RootMeanSquareError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MachineLearningHw2.ErrorCalculation
+{
+	public static class RootMeanSquareError
+	{
+		public static double Calculate(List<MoviePrediction> predictions)
+		{
+			double numerator = 0;
+			foreach (var moviePrediction in predictions)
+			{
+				double diff = moviePrediction.RealRating - moviePrediction.Prediction;
+				numerator += diff*diff;
+			}
+
+			return Math.Sqrt(numerator / predictions.Count);
+		}
+	}
+}
